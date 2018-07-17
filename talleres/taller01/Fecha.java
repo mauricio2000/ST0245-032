a39e1e00b9ecd735e5f51f83fe60f5ec71360ee2
mauricio2000 <41337819+mauricio2000@users.noreply.github.com>
2018-07-17 12:01:11 -0500

    Add files via upload

--- talleres/taller01/Fecha.java
+++ talleres/taller01/Fecha.java
@@ -1,28 +1,101 @@
+
+/**
+ * La clase Fecha tiene la intención de representar el tipo abstracto fecha.
+ * Aunque ya existen librerías encargadas de esta funcion:
+ * @see <a href="http://www.baeldung.com/java-8-date-time-intro"> Ver documentacion tipo Time </a>
+ * En este ejercicio podemos tener una compresión más profunda de su funcionamiento interno.
+ * 
+ * @author Mauricio Toro, Andres Paez
+ * @version 1
+ */
+
 public class Fecha {
-    public Fecha() {
+    
 
+    /*
+    varibales con atributo final indican que una variable es de tipo 
+    constante, es decir, no admitirá cambios después de su declaración y asignación de valor.
+    final determina que un atributo no puede ser sobreescrito o redefinido.
+    Se le asigna esta característica para evitar que se sobrescriban valores.
+    tener en cuenta tipado de las 3 variables!.
+    */
+
+    private final int dia;
+    private final int mes;
+    private final int anyo;
+
+
+    /**
+     * Se inicializan las variables globales en el constructor de manera que no posean valores nulos o 0s.
+     */
+    public Fecha(int dia, int mes, int anyo) {
+        this.dia=dia;
+        this.mes=mes;
+        this.anyo=anyo;
     }
 
+    /**
+     * Método para obtener la variable global dia.
+     *
+     * @return el dia
+     */
     public int dia() {
-
+        return dia;
     }
 
+    /**
+     * Método para obtener la variable global mes.
+     *
+     * @return el mes
+     */
     public int mes() {
-
+        return mes;
     }
 
+    /**
+     * Método para obtener la variable global anio.
+     *
+     * @return el año
+     */
     public int anio() {
-
+        return anyo;
     }
 
-    // -1 si esta fecha es anterior a la otra
-    // 0 si son iguales
-    // 1 si esta fecha es posterior a la otra
+    /**
+    * @param otra representa la fecha con la culase va a comparar.
+    *
+    * El método comprar se encarga de devolvernos respuesta a tres posibilidades.
+    * 1: si la fecha es menor que la otra retorna -1.
+    * 2: si la fecha es igual que la otra retorna 0.
+    * 3: si la fecha es mayor que la otra retorna 1.
+    *
+    * @return -1 sí es menor; 0 sí es igual; 1 sí es mayor.
+    *
+    */
+
     public int comparar(Fecha otra) {
 
+        if(anyo==otra.anio() && mes==otra.mes() && dia==otra.dia()){
+            return 0;
+        }else if(anyo<otra.anio()){
+            return -1;
+        }else if (mes<otra.mes()){
+            return -1;
+        }else if (dia<otra.dia()){
+            return -1;
+        }else{
+            return 1;
+        }
     }
 
+
+     /**
+    * toString se encargará de convertir el tipo abstracto fecha en un tipo cadena
+    * para su posterior visualización
+    *
+    * @return una cadena que contiene la fecha
+    */
     public String toString() {
-        
+        return mes+"/"+dia+"/"+anyo;
     }
 }
